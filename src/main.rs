e09507bd755a68408fa1b5b89df6ddfa274dbee5
yaikohi <erik.beem@student.hu.nl>
2021-06-01 15:51:33 +0200

    trial n error

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
-use sha2::{Digest, Sha256, Sha512};
 use std::vec::Vec;
+use sha2::{ Digest, Sha256, Sha512 };
 
 // https://doc.rust-lang.org/std/hash/index.html
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
-struct ExampleCoinBlock {
-    previous_block_hash: &str,
+
+
+struct ExampleCoinBlock <'a> {
+    previous_block_hash: &'a str,
     transaction_list: Vec<String>,
-    block_data: String,
-    block_hash: u64,
 }
 
-impl ExampleCoinBlock {
-    fn create_block_hash(&self) -> GenericArray<u8> {
-        let mut hasher = Sha256::new();
-        hasher.update(self.block_data);
-        let result = hasher.finalize();
-        println!("{:?}", result)
+type BlockHashT<'a> = &'a str;
+
+
+impl ExampleCoinBlock<'_> {
+    pub fn new() -> ExampleCoinBlock<'_> {
+        ExampleCoinBlock {
+            previous_block_hash,
+            transaction_list,
+        }
+    }
+    
+    fn create_block_hash(&self) -> <BlockHashT> {
+        let block_data = &self.transaction_list
+            .push(&self.previous_block_hash)
+            .join("-");
+        let block_hash = Sha256::new()
+            .update(block_data)
+            .finalize();
+        block_hash
     }
 }
+
 fn main() {
-    println!("Hello, world!");
     let first_coin = ExampleCoinBlock {
         previous_block_hash: "Initial string",
         transaction_list: vec!["T1".to_string(), "T2".to_string()],
-        block_data: self.transaction_list.join("-") + "-" + self.previous_block_hash,
-        block_hash: Sha256::new().update(self.block_data).finalize(),
     };
+    println!("{:?}", first_coin.transaction_list)
 }
