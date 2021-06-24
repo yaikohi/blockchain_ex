4fc2c76f1838bc1e09efed1df9cd59b1406839c8
Erik Beem <erik.beem@hotmail.com>
2021-06-24 20:39:47 +0200

    Added docstrings to Blockchain

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -7,15 +7,15 @@ from hashlib import sha256
 
 class Blockchain:
     """The blockchain class containing all the blocks.
-    
+
     Attributes:
         chain: an array containing blocks with Genesisblock index 0.
     """
-    
+
     def __init__(self):
         self.chain = []
         self.create_genesis_block()
- 
+
     def create_genesis_block(self):
         """
         A function to generate a genesis block and append it to
@@ -24,35 +24,31 @@ class Blockchain:
         genesis_block = Block(0, [], time.time(), "0")
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
- 
+
     @property
     def last_block(self):
         """
         A quick pythonic way to retrieve the most recent block in the chain.
         """
         return self.chain[-1]
-    
+
 
 class Block:
-    def __init__(self, index: int, 
-                 transactions: List, 
-                 timestamp: time.time, 
-                 previous_hash: str):
+    def __init__(self):
         """ Constructor for the `Block` class.
-        
+
         Args:
             index: Unique ID of the block.
             transactions: List of transactions.
             timestamp: Time of generation of the block.
-            previous_hash: Hash of the previous block in the chain which this block is part of.                                        
-        
+            previous_hash: Hash of the previous block in the chain which this block is part of.
+
         Returns:
             An instance of the `Block` class. A block object.
         """
-        self.index = index
-        self.transactions = transactions
-        self.timestamp = timestamp
-        self.previous_hash = previous_hash # Adding the previous hash field
+        self.verified_transactions = []
+        self.previous_block_hash = ""
+        self.Nonce = ""
  
     def compute_hash(self):
         """
